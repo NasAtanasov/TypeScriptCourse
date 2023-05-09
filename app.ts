5d0e55bf057e77a6704f612c4032612165da54b7
Nas <nasatanasov84@gmail.com>
2023-05-10 00:36:38 +0300

    Watch mode | Compiling entire project | include and exclude files in tsconfig.json

--- app.ts
+++ app.ts
@@ -1,16 +1,3 @@
-let userInput: unknown;
-let userName: string;
+const userName = 'Maxi';
 
-userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-
-function generateError(message:string, code: number): never {
-    throw {message: message, errorCode: code};
-    //while(true){}
-}
-
-const result = generateError('An error Occurred!', 500);
-console.log(result);
+console.log(userName);
--- basic.ts
+++ basic.ts
@@ -1,4 +1,4 @@
-function add (n1: number, n2: number, showResult: boolean, phrase: string) {
+function addBasicTs (n1: number, n2: number, showResult: boolean, phrase: string) {
     // console.log(typeof number1)
     // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
     //     throw new Error('Incorrect input!');
@@ -13,8 +13,8 @@ function add (n1: number, n2: number, showResult: boolean, phrase: string) {
 let number1 : number;
 number1 = 5;
 const number2 = 2.8;
-const printResult = true;
+const printResultBasicTs = true;
 const resultPhrase = 'Result is: ';
 
-const result = add(number1, number2, printResult, resultPhrase);
+const resultBasicTs = addBasicTs(number1, number2, printResultBasicTs, resultPhrase);
 console.log(result);
