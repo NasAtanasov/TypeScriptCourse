2101374570e254907dfb9cf4b08e7319b63c1764
Nas <nasatanasov84@gmail.com>
2023-07-10 23:15:18 +0300

    Inheritance

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ class Department {
     // private name: string;
     private employees: string[] = [];
 
-    constructor(private readonly id: string, private name: string) {
+    constructor(private readonly id: string, public name: string) {
         // this.id = id;
         // this.name = n;
     }
@@ -24,14 +24,44 @@ class Department {
     }
 }
 
-const accounting = new Department('d1', 'Accounting');
+class ITDepartment extends Department {
+    constructor(id: string, public admins: string[]) {
+        super(id, 'IT',);
+    }
+}
 
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
-accounting.addEmployee('Anna');
+class AccountingDepartment extends Department {
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting');
+    }
 
-accounting.describe();
-accounting.printEmployeeInformation();
+    addReports(text: string) {
+        this.reports.push(text);
+    }
+
+    printReports() {
+        console.log(this.reports);
+    }
+}
+
+const it = new ITDepartment('d1', ['Nas']);
+
+it.addEmployee('Max');
+it.addEmployee('Manu');
+
+// it.employees[2] = 'Anna';
+
+it.describe();
+it.name = 'NEW NAME';
+it.printEmployeeInformation();
+
+console.log(it);
+
+const accounting = new AccountingDepartment('d2', []);
+
+accounting.addReports('Someone went worng...');
+
+accounting.printReports();
 
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
