dfbf739bef4f34b9877075de3136b277353f991f
Nas <nasatanasov84@gmail.com>
2024-03-21 22:46:26 +0200

    :hammer: interfaces with classes

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,25 @@
-interface Person {
+interface Greetable {
     name: string;
-    age: number;
 
     greet(phrase: string) :void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+    name: string;
+    age = 30;
 
-user1 = {
-    name: 'Max',
-    age: 30,
-    greet(phrase: string) {
+    constructor(n: string) {
+        this.name = n;
+    }
+
+    greet(phrase: string): void {
         console.log(phrase + ' ' + this.name)
     }
-};
+}
 
-user1.greet('Hi there - I\'m')
+let user1: Greetable;
+
+user1 = new Person('Max');
+
+user1.greet('Hi there - I\'m');
+console.log(user1)
