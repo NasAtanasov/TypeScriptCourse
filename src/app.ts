03c4282fab3eb36b188e1c7fd3468b89a2ce6515
Nas <nasatanasov84@gmail.com>
2024-03-27 23:39:53 +0200

    Another Generic Function

--- src/app.ts
+++ src/app.ts
@@ -25,6 +25,25 @@ console.log(mergeObj.age);
 console.log(mergeObj.name);
 console.log(mergeObj.hobbies);
 
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+    let descriptionText = 'Got no value';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element';
+    } else if (element.length > 1) {
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+    return [element, descriptionText]
+}
+
+console.log(countAndDescribe(''));
+console.log(countAndDescribe(['123', '123', '123']));
+console.log(countAndDescribe('H'));
+console.log(countAndDescribe('Hi there!'));
+
 
 
 
