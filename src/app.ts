3a461890b068021ee17dd82283cec499c73324dc
Nas <nasatanasov84@gmail.com>
2024-04-06 22:43:54 +0300

    Example: Creating an 'Autobin' Decorator

--- src/app.ts
+++ src/app.ts
@@ -61,7 +61,11 @@ function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
 
 }
 
-function Log3(target: any, name: string, descriptor: PropertyDescriptor) {
+function Log3(
+    target: any,
+    name: string | Symbol,
+    descriptor: PropertyDescriptor
+) {
     console.log('Method decorator! 3');
     console.log(target);
     console.log(name);
@@ -102,3 +106,30 @@ class Product {
 
 const p1 = new Product('Book', 19);
 const p2 = new Product('Book 2', 21);
+
+function Autobind(_:any, _2: string | Symbol, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+class Printer {
+    message = 'This works!';
+
+    @Autobind
+    showMessage() {
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage)
