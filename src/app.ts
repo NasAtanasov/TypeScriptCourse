5ea0c801c0502125b12f3bda0112ae0419cd34d8
Nas <nasatanasov84@gmail.com>
2024-06-30 02:32:39 +0300

    TypeScript-embracing: class-validator

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import "reflect-metadata";
 import { plainToInstance } from "class-transformer";
+import { validate } from 'class-validator';
+
 import { Product } from './product.model';
 
 
 const products = [{title: 'A Carpet', price: 29.99}, {title: 'A Book', price: 12.99}];
+
+const newProd = new Product('', -5.99);
+validate(newProd).then(errors => {
+    if (errors.length > 0) {
+        console.log('[1;32m Validation Errors![0m');
+        console.log('[1;31m ---------- ' + errors + '[0m');
+    } else {
+        console.log(newProd.getInformation());
+    }
+});
 
 //const p1 = new Product('A Book', 12.99);
 
