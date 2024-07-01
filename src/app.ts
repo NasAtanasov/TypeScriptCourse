d6e37b5104db2c03b0dc45192db44044196316fb
Nas <nasatanasov84@gmail.com>
2024-07-02 00:02:09 +0300

    Using Axios to Fetch Coordinates for an Entered Address

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,36 @@
-const form =document.querySelector('form');
-const addressInput = document.getElementById('address')! as HTMLInputElement;
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyDNpzr1hqqE-6KxpOO2PM9MKWeLhky-418';
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 
 function searchAddressHandler(event: Event) {
-    event.preventDefault();
-    const enterAddress = addressInput.value;
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then(response => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      console.log('Coordinates:', coordinates);
+    })
+    .catch(err => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
-form.addEventListener('submit', searchAddressHandler);
+form.addEventListener("submit", searchAddressHandler);
